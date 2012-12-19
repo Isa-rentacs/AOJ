0f70ac10fa4e0efe8473461d00433fda24f19624
Isa_rentacs <jkuroda.isa@gmail.com>
2012-12-20 01:27:53 +0900

    added _skelton

---
+++ _skelton.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctime>
+
+using namespace std;
+
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define PB push_back
+#define MP make_pair
+#define ll long long
+
+inline int toInt(string s){int v;istringstream sin(s);sin>>v;return v;}
+template<class T> inline string toString(T x){ostringstream sout;sout<<x;return sout.str();}
+
+int main(){
+    std::ios::sync_with_stdio(false);
+}
